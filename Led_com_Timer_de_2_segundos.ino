32fa9b529361d8e29167a4fbb67f79e1e78f8f0a
Mateus Martins Ferreira <mateusmferreira@live.com>
2017-10-18 06:57:45 -0200

    Add files via upload

---
+++ Led_com_Timer_de_2_segundos.ino
@@ -0,0 +1,25 @@
+#define LED1 40
+#define LED2 38
+#define LED3 36
+
+void setup() {
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+  pinMode(LED3,OUTPUT);
+
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3,LOW);
+}
+
+void loop() {
+  digitalWrite(LED3,HIGH);
+  delay(2000);
+  digitalWrite(LED3,LOW);
+  digitalWrite(LED2,HIGH);
+  delay(2000);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED1,HIGH);
+  delay(2000);
+  digitalWrite(LED1,LOW);
+}
---
+++ distanci.ino
@@ -0,0 +1,40 @@
+//Programa: Conectando Sensor Ultrassonico HC-SR04 ao Arduino
+//Autor: FILIPEFLOP
+
+//Carrega a biblioteca do sensor ultrassonico
+#include <Ultrasonic.h>
+
+//Define os pinos para o trigger e echo
+#define pino_trigger 4
+#define pino_echo 5
+#define NOTE_D4  294
+
+int pinoAudio = 6;
+
+//Inicializa o sensor nos pinos definidos acima
+Ultrasonic ultrasonic(pino_trigger, pino_echo);
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Lendo dados do sensor...");
+}
+
+void loop()
+{
+  
+  //Le as informacoes do sensor, em cm e pol
+  float cmMsec, inMsec;
+  long microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  //Exibe informacoes no serial monitor
+
+  if( cmMsec < 30.0 ){
+    tone(pinoAudio, NOTE_D4);
+    delay(1000);
+  }
+  noTone(pinoAudio);
+  Serial.print("Distancia em cm: ");
+  Serial.println(cmMsec);
+  delay(1000);
+}
